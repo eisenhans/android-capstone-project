18c899bd3583743820192bfccdf95f69fbcb5803
Markus Loeffler <markus.loeffler@allianz-assistance.de>
2016-02-16 15:43:15 +0000

    add libraries

--- RememberMe/app/src/main/java/com/gmail/maloef/rememberme/domain/Word.java
+++ RememberMe/app/src/main/java/com/gmail/maloef/rememberme/domain/Word.java
@@ -1,17 +1,14 @@
 package com.gmail.maloef.rememberme.domain;
 
-import java.util.Date;
-
 public class Word {
 
     String foreignLanguage;
     String nativeLanguage;
     int compartment;
 
-    // use long?
-    Date creationDate;
-    Date updateDate;
-    Date lastRepeatDate;
+    long creationDate;
+    long updateDate;
+    long lastRepeatDate;
 
 
 }
