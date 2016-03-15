713ca3674d5940506c349730cc6f8a0df9c99495
Markus Loeffler <markus.loeffler@allianz-assistance.de>
2016-03-15 13:33:10 +0100

    no multiline in dialogs

--- RememberMe/app/src/main/java/com/gmail/maloef/rememberme/util/dialog/ValidatingInputDialog.java
+++ RememberMe/app/src/main/java/com/gmail/maloef/rememberme/util/dialog/ValidatingInputDialog.java
@@ -5,7 +5,11 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.gmail.maloef.rememberme.R;
 
@@ -27,6 +31,8 @@ public class ValidatingInputDialog {
         this.inputValidator = inputValidator;
 
         final EditText editText = new EditText(activity);
+        editText.setSingleLine();
+        editText.setImeOptions(EditorInfo.IME_ACTION_DONE);
         alertDialogBuilder.setView(editText);
 
         alertDialogBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
@@ -38,7 +44,23 @@ public class ValidatingInputDialog {
         alertDialogBuilder.setNegativeButton(R.string.cancel, null);
 
         alertDialog = alertDialogBuilder.create();
+        editText.setOnEditorActionListener(createDoneListener());
         editText.addTextChangedListener(createTextWatcher());
+    }
+
+    private TextView.OnEditorActionListener createDoneListener() {
+        return new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    if (inputValidator.isValid(v.getText().toString())) {
+                        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).performClick();
+                    }
+                    return true;
+                }
+                return false;
+            }
+        };
     }
 
     private TextWatcher createTextWatcher() {
@@ -51,6 +73,7 @@ public class ValidatingInputDialog {
 
             @Override
             public void afterTextChanged(Editable s) {
+                logInfo("afterTextChanged, editable now is " + s);
                 String input = s.toString();
                 boolean inputOk = inputValidator.isValid(input);
                 alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(inputOk);
@@ -61,5 +84,9 @@ public class ValidatingInputDialog {
     public void show() {
         alertDialog.show();
         alertDialog.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(false);
+    }
+
+    void logInfo(String message) {
+        Log.i(getClass().getSimpleName(), message);
     }
 }
